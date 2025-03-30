13d2cbfae1e52948b07850af42a4893a9fc4a37d
Elfredah <83861738+Elfredah@users.noreply.github.com>
2025-03-30 03:08:24 +0100

    Update median.test.js

--- Sprint-1/fix/median.test.js
+++ Sprint-1/fix/median.test.js
@@ -24,3 +24,26 @@ describe("calculateMedian", () => {
     expect(list).toEqual([1, 2, 3]);
   });
 });
+
+
+
+const calculateMedian = require("./median.js");
+
+describe("calculateMedian", () => {
+  test("returns the median for odd length array", () => {
+    expect(calculateMedian([1, 2, 3])).toEqual(2); // Odd length, median is 2
+    expect(calculateMedian([1, 2, 3, 4, 5])).toEqual(3); // Odd length, median is 3
+  });
+
+  test("returns the average of middle values for even length array", () => {
+    expect(calculateMedian([1, 2, 3, 4])).toEqual(2.5); // Even length, median is average of 2 and 3
+    expect(calculateMedian([1, 2, 3, 4, 5, 6])).toEqual(3.5); // Even length, median is average of 3 and 4
+  });
+
+  test("doesn't modify the input", () => {
+    const list = [1, 2, 3];
+    calculateMedian(list);
+
+    expect(list).toEqual([1, 2, 3]); // Ensure the original list remains unchanged
+  });
+});
