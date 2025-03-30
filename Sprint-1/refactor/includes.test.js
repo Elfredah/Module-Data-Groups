932b59b9886387735637932f5acd0cef6dbfc558
Elfredah <83861738+Elfredah@users.noreply.github.com>
2025-03-30 03:27:43 +0100

    Update includes.test.js

--- Sprint-1/refactor/includes.test.js
+++ Sprint-1/refactor/includes.test.js
@@ -36,3 +36,43 @@ test("searches for null", () => {
 
   expect(currentOutput).toEqual(targetOutput);
 });
+
+
+//  sOLUTIONS
+const includes = require("./includes.js");
+
+test("returns true when target is in array", () => {
+  const currentOutput = includes(["a", "b", "c", "d"], "c");
+  const targetOutput = true;
+
+  expect(currentOutput).toEqual(targetOutput);
+});
+
+test("returns false when target not in array", () => {
+  const currentOutput = includes([1, 2, 3, 4], "a");
+  const targetOutput = false;
+
+  expect(currentOutput).toEqual(targetOutput);
+});
+
+test("returns true when the target is in array multiple times", () => {
+  const currentOutput = includes([1, 2, 2, 3], 2);
+  const targetOutput = true;
+
+  expect(currentOutput).toEqual(targetOutput);
+});
+
+test("returns false for empty array", () => {
+  const currentOutput = includes([], "a"); // passing the target value here
+  const targetOutput = false;
+
+  expect(currentOutput).toEqual(targetOutput);
+});
+
+test("searches for null", () => {
+  const currentOutput = includes(["b", "z", null, "a"], null);
+  const targetOutput = true;
+
+  expect(currentOutput).toEqual(targetOutput);
+});
+
